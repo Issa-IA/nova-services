17e875c70395c73f79f30c1d99956806184d588d
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-09-26 15:01:34 +0100

    Update models.py

--- venteADD1/models/models.py
+++ venteADD1/models/models.py
@@ -36,6 +36,19 @@ class SaleOrderLineHerit(models.Model):
 
 class SaleOrderHerit(models.Model):
     _inherit    = 'sale.order'
+    
+    #new montant total de rachat
+    sale_total_rachat = fields.Float(string='Montant de rachat total',default=0.0,compute="sale_total_rachat_func")
+    @api.onchange("sale_bonretour")
+    def sale_total_rachat_func(self):
+        for rec in self:
+            somme = 0
+            if rec.sale_bonretour:
+                for retour in rec.sale_bonretour:
+                        somme+=retour.bonretour_montant
+            rec.sale_total_rachat = somme
+    #fin montant de rachat
+    
     sale_total_vente = fields.Monetary(string="Total vente",default=0.0,compute="sale_total_vente_func")
     sale_marge  = fields.Monetary(compute="sale_marge_fuc",default=0.0, string="Marge commerciale")
     sale_total_achat = fields.Monetary(string="Total vente", default=0.0, compute="sale_total_achat_func")
@@ -205,17 +218,17 @@ class SaleOrderHerit(models.Model):
     sale_abonnement_service = fields.Monetary(string="Abonnement Service")
     sale_autre_frais        = fields.Monetary(string="Autre frais")
 
-    @api.onchange("sale_montatnt_IR","sale_total_vente","sale_finance","sale_frais","sale_frais_restitution","sale_vr_client","sale_ir_prospects","sale_vr_client_2","sale_rachat_matriel","sale_Gratuite","sale_partenariat","sale_solde_2_fois")
+    @api.onchange("sale_total_rachat","sale_montatnt_IR","sale_total_vente","sale_finance","sale_frais","sale_frais_restitution","sale_vr_client","sale_ir_prospects","sale_vr_client_2","sale_rachat_matriel","sale_Gratuite","sale_partenariat","sale_solde_2_fois")
     def sale_marge_fuc(self):
         for rec in self:
-            rec.sale_marge = rec.sale_finance-rec.sale_montatnt_IR + rec.sale_frais -rec.sale_total_vente- rec.sale_frais_restitution -rec.sale_vr_client-rec.sale_ir_prospects-rec.sale_vr_client_2-rec.sale_rachat_matriel-rec.sale_Gratuite-rec.sale_partenariat-rec.sale_solde_2_fois
+            rec.sale_marge = rec.sale_finance- rec.sale_total_rachat-rec.sale_montatnt_IR + rec.sale_frais -rec.sale_total_vente- rec.sale_frais_restitution -rec.sale_vr_client-rec.sale_ir_prospects-rec.sale_vr_client_2-rec.sale_rachat_matriel-rec.sale_Gratuite-rec.sale_partenariat-rec.sale_solde_2_fois
 
-    @api.onchange("sale_montatnt_IR","sale_total_achat", "sale_finance", "sale_frais", "sale_frais_restitution", "sale_vr_client",
+    @api.onchange("sale_total_rachat","sale_montatnt_IR","sale_total_achat", "sale_finance", "sale_frais", "sale_frais_restitution", "sale_vr_client",
                   "sale_ir_prospects", "sale_vr_client_2", "sale_rachat_matriel", "sale_Gratuite", "sale_partenariat",
                   "sale_solde_2_fois")
     def sale_marge_reel_fuc(self):
         for rec in self:
-            rec.sale_marge_reel = rec.sale_finance -rec.sale_montatnt_IR + rec.sale_frais - rec.sale_total_achat - rec.sale_frais_restitution - rec.sale_vr_client - rec.sale_ir_prospects - rec.sale_vr_client_2 - rec.sale_rachat_matriel - rec.sale_Gratuite - rec.sale_partenariat - rec.sale_solde_2_fois
+            rec.sale_marge_reel =rec.sale_total_rachat+ rec.sale_finance -rec.sale_montatnt_IR + rec.sale_frais - rec.sale_total_achat - rec.sale_frais_restitution - rec.sale_vr_client - rec.sale_ir_prospects - rec.sale_vr_client_2 - rec.sale_rachat_matriel - rec.sale_Gratuite - rec.sale_partenariat - rec.sale_solde_2_fois
 
     @api.depends("order_line")
     def sale_total_vente_func(self):
