1dc887e82875d44236103a74fa89db640f6fdc64
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2022-10-03 15:54:35 +0100

    Update partner_invoice_inherit.py

--- partner_invoice/model/partner_invoice_inherit.py
+++ partner_invoice/model/partner_invoice_inherit.py
@@ -11,13 +11,4 @@ class PartnerInvoiceHerit(models.Model):
     type_facture = fields.Selection([('par_dossier', 'facture par dossier'),('tout_dossiers', 'facturer tout les dossiers')],default='par_dossier')
     moyen_de_paiement= fields.Selection([('prelevement', 'Prélèvement'),('chorus', 'Chorus'),('autres', 'Autres')],default='prelevement')
     
-    def write(self, vals):
-        res = super(PartnerInvoiceHerit, self).write(vals)
-        for rec in self:
-           rec.x_studio_group_ok = rec.env.user.has_group('droits_d_acces.group_Technicien_contact')
-        return res
-
-
-
-
-
+ 
