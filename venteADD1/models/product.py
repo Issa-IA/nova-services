60bc32d96b6957dd06f0f45674130b778c553c44
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2022-10-14 13:35:30 +0100

    Update product.py

--- venteADD1/models/product.py
+++ venteADD1/models/product.py
@@ -8,6 +8,15 @@ class ProductProductInherit(models.Model):
     product_marque = fields.Many2one("fleet.vehicle.model.brand", string='Marque', related="product_Modele.brand_id")
     product_Modele = fields.Many2one("fleet.vehicle.model", string='Modèle')
     product_type = fields.Char(string='Type', compute="product_type_compute")
+    product_type1 = fields.Char(string='Type', compute="product_type_compute1")
+    
+    @api.onchange('product_Modele')
+    def product_type_compute1(self):
+        for rec in self:
+            if rec.product_Modele.type_materiels:
+                    rec.product_type1 = str(rec.product_Modele.type_materiels)
+            else:
+                rec.product_type1 = False
 
     @api.onchange('product_Modele')
     def product_type_compute(self):
